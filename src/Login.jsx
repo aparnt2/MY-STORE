387c5fa42f448c4ad97ec8e7dca19b729bc3bac4
aparnt2 <aparnabrq@gmail.com>
2026-04-17 04:17:19 -0700

    fix login css path

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import "./login.css";
+import "./Login.css";
 import { Link } from "react-router-dom";
 
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
